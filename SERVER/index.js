27a66800eb607a8c6b12ecd89719c20280c05d99
Aman Patel <amanpatel.ajp@gmail.com>
2024-10-18 12:59:17 +0530

    Handling request methods with route

--- SERVER/index.js
+++ SERVER/index.js
@@ -4,7 +4,7 @@ const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}: ${req.url} New Req Recieved\n`;
+  const log = `${Date.now()}: ${req.method} ${req.url} New Req Recieved\n`;
   const myUrl = url.parse(req.url, true);
   console.log(myUrl);
 
@@ -13,19 +13,24 @@ const myServer = http.createServer((req, res) => {
     // switch(req.url)
     switch (myUrl.pathname) {
       case "/":
-        res.end("HomePage");
+        if (req.method === "GET") res.end("HomePage");
         break;
       case "/about":
         const username = myUrl.query.myname;
         res.end(`Hi, ${username}`);
         // res.end("I am Aman Patel");
         break;
-
       case "/search":
-       const search = myUrl.query.search_quary;
+        const search = myUrl.query.search_quary;
         res.end("Here are your results for " + search);
         break;
-
+      case "/signup":
+        if (req.method === "GET") res.end("this is a signup form");
+        else if (req.method === "POST") {
+          //db query
+          res.end("Success");
+        }
+        break;
       default:
         res.end("404 Not Found");
     }
